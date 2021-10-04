1c8faa04df7419b1e24606fb96441608e1bbfa7f
theonlydvr <theonlydvr@gmail.com>
2021-10-03 22:23:51 -0500

    JS Contact form

---
+++ assets/js/contact.js
@@ -0,0 +1,27 @@
+$('#contact-form').on('submit',function(e){
+        //optional validation code here
+
+        e.preventDefault();
+
+        $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbz-R7x6vmu0HKbNkqooJQfD3pFcY5mqtgOyDoyzgI2wMLhWWDmSGFrW2089EoeKZ6y6/exec",
+            method: "POST",
+            dataType: "json",
+            data: $("#contact-form").serialize(),
+            success: function(response) {
+
+                if(response.result == "success") {
+                    $('#contact-form')[0].reset();
+                    alert('Thank you for contacting me!');
+                    return true;
+                }
+                else {
+                    alert("Something went wrong. Please try again.")
+                }
+            },
+            error: function() {
+
+                alert("Something went wrong. Please try again.")
+            }
+        })
+    });
